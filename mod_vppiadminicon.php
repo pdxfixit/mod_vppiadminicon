01796e94caeb3364541973dbe11587e59186f9b3
Curtis BrandSmith <jcbrandsmith@gmail.com>
2014-09-09 15:13:46 -0700

    Basic changes and added gitignore file

--- mod_vppiadminicon.php
+++ mod_vppiadminicon.php
@@ -1,20 +1,21 @@
 <?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_vppiadminicon
+ *
+ * @copyright   Copyright (C) 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 // no direct access
 defined('_JEXEC') or die;
 
-//get params
-$db = JFactory::getDbo();
-$query = $db->getQuery(true)->select('*')->from($db->qn('#__lfcoursemanager_settings'));
-$settings = $db->setQuery($query, 0, 1)->loadObject();
-$sugarParams = json_decode($settings->params)->survey_settings;
-$key = $sugarParams->crnkey;
-unset($db, $query, $settings, $sugarParams);
 ?>
 <div id="cpanel">
     <div>
         <div class="icon">
-            <a href="/index.php?option=com_lfcoursemanager&view=sugarimport&key=<?php echo $key; ?>" target="_blank">
-                <i class="icon-cube"></i> <span><?php echo JText::_('Run Sugar CRM Import'); ?></span></a>
+            <a href="/index.php?option=com_vppi&view=homes">
+                <i class="icon-cube"></i> <span><?php echo JText::_('Manage Homes'); ?></span></a>
         </div>
     </div>
 </div>
