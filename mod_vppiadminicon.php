36f9e152c5bebc557746ebfec80bbee46b5eba8a
Curtis BrandSmith <jcbrandsmith@gmail.com>
2014-09-09 18:24:46 -0700

    fixed the url

--- mod_vppiadminicon.php
+++ mod_vppiadminicon.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
 <div id="vppi-link" class="btn-large">
     <div>
         <div class="icon">
-            <a href="/index.php?option=com_vppi&view=homes" style="color: #6e7e29;">
+            <a href="/administrator/index.php?option=com_vppi" style="color: #6e7e29;">
                 <i class="icon-home" style="color: #6e7e29;"></i> <span>&nbsp;&nbsp;<?php echo JText::_('Manage Properties'); ?></span></a>
         </div>
     </div>
